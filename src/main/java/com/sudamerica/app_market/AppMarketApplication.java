cb0f7dc617d42bdf7dc64969a0839e5acf592480
PRE VERA BAJANA SAMUEL JOSIA <svera@sasf.local>
2025-10-31 16:13:10 -0500

    Agregado mappers Producto to Product

--- src/main/java/com/sudamerica/app_market/AppMarketApplication.java
+++ src/main/java/com/sudamerica/app_market/AppMarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppMarketApplication {
-	
+
 	/* Comentario agregado*/
 
 	public static void main(String[] args) {
--- src/main/java/com/sudamerica/app_market/persistence/mapper/CategoryMapper.java
+++ src/main/java/com/sudamerica/app_market/persistence/mapper/CategoryMapper.java
@@ -1,7 +1,25 @@
 package com.sudamerica.app_market.persistence.mapper;
 
+import com.sudamerica.app_market.domain.Category;
+import com.sudamerica.app_market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+    })
 
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos",ignore = true)
+    Categoria toCategoria(Category category);
 
 }
